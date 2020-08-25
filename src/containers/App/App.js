a2d9b982eed26224202fbaeefbc4809ff5c5df35
PajkoStefan <52094853+PajkoStefan@users.noreply.github.com>
2020-08-26 00:58:15 +0200

    React-Bootstrap installed

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 import Layout from "../../components/Layout/Layout";
 
+//Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {

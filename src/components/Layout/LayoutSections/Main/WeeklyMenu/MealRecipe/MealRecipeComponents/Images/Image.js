f3bd73bea7e6f774e40a09db82c5a80a2ccfbb75
PajkoStefan <anastasovski.stefan96@gmail.com>
2021-08-12 01:55:13 +0200

    Image - bug - fixed

--- src/components/Layout/LayoutSections/Main/WeeklyMenu/MealRecipe/MealRecipeComponents/Images/Image.js
+++ src/components/Layout/LayoutSections/Main/WeeklyMenu/MealRecipe/MealRecipeComponents/Images/Image.js
@@ -3,17 +3,19 @@ import React from "react";
 const Image = (props) => {
 
     let url;
-    if(props.img.url){
+    if(props.img && props.img.url){
         url = props.img.url
-    }else{
+    }else if(props.img) {
         url = props.img
+    } else {
+        url = ""
     }
 
     return (
 
         <img className={props.className}
              src={url}
-             alt={props.img.alt}
+             alt={(props.img && props.img.alt) || "ERROR"}
              width="100%"
              height="100%"
         />
